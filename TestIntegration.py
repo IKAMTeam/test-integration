02ad69cef001314b718b4ccff5948f84239c2959
Ilya Emelyanov <emel.9616@gmail.com>
2020-07-03 20:18:49 +0700

    IHub-158712 Add a file with Integration parameters to the integration directory

--- TestIntegration.py
+++ TestIntegration.py
@@ -10,7 +10,9 @@ site = passwordData["URL"]
 trackorType = passwordData["TrackorType"]
 trackorKey = passwordData["TrackorKey"]
 
-with open('ihub_process_id', "rb") as PFile:
-    processId = PFile.read().decode('utf-8')
+with open('ihub_parameters.json', "rb") as PFile:
+    ihub_data = json.loads(PFile.read().decode('utf-8'))
+
+processId = ihub_data['processId']
 
 integrationOV = OVIntegration.OVIntegration(url=site, userName=user, password=password, trackorType=trackorType, trackorKey=trackorKey, processId = processId)
